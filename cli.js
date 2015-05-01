1b15d00800d738faa1562d1b1439dcf4d67a4a0c
ButuzGOL <ButuzGOL@gmail.com>
2015-05-01 14:24:03 +0300

    Create command

---
+++ cli.js
@@ -0,0 +1,82 @@
+#!/usr/bin/env node
+
+'use strict';
+
+process.bin = process.title = 'gdem';
+
+var fs = require('fs');
+var program = require('commander');
+var inquirer = require('inquirer');
+
+var pkg = require('./package.json');
+
+var rootFolderPath = getUserHome() + '/.gdem';
+
+function getUserHome() {
+  return process.env.HOME || process.env.USERPROFILE;
+}
+
+function createRootFolder() {
+  if (!fs.existsSync(rootFolderPath))
+    fs.mkdirSync(rootFolderPath);
+}
+
+function create(answers) {
+  var projectFolderPath = rootFolderPath + '/' + answers.name;
+  var data;
+
+  fs.mkdirSync(projectFolderPath);
+
+  data = {
+    serviceAccountEmail: answers.serviceAccountEmail,
+    pathToKeyFile: answers.pathToKeyFile,
+    gitPath: answers.gitPath,
+    buildCommand: answers.buildCommand
+  };
+
+  fs.writeFileSync(projectFolderPath + '/.gdem',
+    JSON.stringify(data, null, 2));
+}
+
+function createCommand() {
+  inquirer.prompt([{
+    type: 'input',
+    name: 'name',
+    message: 'Name'
+  }, {
+    type: 'input',
+    name: 'serviceAccountEmail',
+    message: 'Service account email'
+  }, {
+    type: 'input',
+    name: 'pathToKeyFile',
+    message: 'Path to key file'
+  }, {
+    type: 'input',
+    name: 'gitPath',
+    message: 'Git path'
+  }, {
+    type: 'input',
+    name: 'buildCommand',
+    message: 'Build command'
+  }], function(answers) {
+    create(answers);
+  });
+}
+
+
+
+createRootFolder();
+
+program
+  .version(pkg.version)
+  .usage('<command> [<args>]');
+  // .command('remove [name]', 'remove environment')
+  // .command('update', 'update current environment')
+  // .command('publish [cursor]', 'publish based on cursor (branch, teg, hash)');
+
+program
+  .command('<create>', 'create environment')
+  .action(createCommand);
+
+program.parse(process.argv);
--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 'use strict';
 
+// gdem create
+// prompt:
+// 1. Name
+// 2. Service account -> Email address:
+// 3. Path to pem file
+// 4. Git path
+// 5. Folder name on gd
+// 6. build command
+// gdem use `project`
+// gdem remove `project`
+// gdem update
+// prompt: ... same as in create but with values
+// gdem publish `master`
+// steps:
+// git pull
+// git checkout master
+// run `build command`
+// publish on gd
+
 module.exports = function() {
 };
